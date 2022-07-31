6f6014aba10a70ef8ecbc5f5e17ea00b353ad9da
rua1hc <tatut90@gmail.com>
2022-07-31 16:38:33 +0700

    setup items from start-up

--- js/to-do.js
+++ js/to-do.js
@@ -2,12 +2,26 @@ const addInput = document.getElementById("add__input");
 const addBtn = document.getElementById("add__btn");
 const todoBoard = document.getElementById("todo__board");
 
+window.addEventListener("DOMContentLoaded", setupTask);
+
 addBtn.addEventListener("click", function () {
+    const id = new Date().getTime().toString();
+
+    if (!addInput.value) alert("Pls. input the task");
+    else {
+        createTask(id, addInput.value);
+        addLocalStorage(id, addInput.value);
+        addInput.value = "";
+    }
+});
+
+function createTask(id, val) {
     let newItem = document.createElement("div");
     newItem.classList.add("task");
+    newItem.setAttribute("task-id", id);
 
     let taskContent = document.createElement("div");
-    taskContent.innerText = `${addInput.value}`;
+    taskContent.innerText = val;
     taskContent.classList.add("task__content");
     newItem.appendChild(taskContent);
 
@@ -21,16 +35,44 @@ addBtn.addEventListener("click", function () {
     deleteBtn.classList.add("delete__task");
     newItem.appendChild(deleteBtn);
 
-    if (!addInput.value) alert("Pls. input the task");
-    else {
-        todoBoard.appendChild(newItem);
-        addInput.value = "";
-    }
-
     checkBtn.addEventListener("click", function () {
         taskContent.classList.toggle("clicked");
     });
     deleteBtn.addEventListener("click", function () {
         todoBoard.removeChild(newItem);
+        removeFromLocalStorage(id);
     });
-});
+
+    todoBoard.appendChild(newItem);
+}
+
+// ***** INITIAL SETUP *********
+function setupTask() {
+    const taskList = getLocalStorage();
+    if (taskList.length !== 0) {
+        taskList.forEach((task) => {
+            createTask(task.id, task.val);
+        });
+    }
+    console.log(taskList);
+}
+
+// ***** LOCAL STORAGE *********
+function addLocalStorage(id, val) {
+    const task = { id, val };
+    let taskList = getLocalStorage();
+    taskList.push(task);
+    localStorage.setItem("task-list", JSON.stringify(taskList));
+}
+
+function removeFromLocalStorage(id) {
+    let taskList = getLocalStorage();
+    taskList = taskList.filter((task) => task.id !== id);
+    localStorage.setItem("task-list", JSON.stringify(taskList));
+}
+
+function getLocalStorage() {
+    return localStorage.getItem("task-list")
+        ? JSON.parse(localStorage.getItem("task-list"))
+        : [];
+}
